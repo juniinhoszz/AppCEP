d62d1eb91c919b0bfcb52960682145f45ce92bb0
Usuario <Usuario@CPD02-M08>
2023-02-10 14:36:13 -0300

    .

---
+++ AppCEP/AppCEP/Model/Cidade.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppCEP.Model
+{
+    public class Cidade
+    {
+        public int id_cidade { get; set; }
+        public string descricao { get; set; }
+        public string uf { get; set; }
+        public int codigo_ibge { get; set; }
+        public int ddd { get; set; }
+    }
+}

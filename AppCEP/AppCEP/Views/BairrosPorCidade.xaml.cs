19a1a7ba9f4ff6bd7ab6664932ffafc48ffdb829
Usuario <Usuario@CPD02-M08>
2023-02-23 15:26:18 -0300

    commit

--- AppCEP/AppCEP/Views/BairrosPorCidade.xaml.cs
+++ AppCEP/AppCEP/Views/BairrosPorCidade.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using AppCEP.Model;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using AppCEP.Service;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,14 +14,37 @@ namespace AppCEP.View
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class BairrosPorCidade : ContentPage
     {
+        ObservableCollection<Cidade> lista_cidades = new ObservableCollection<Cidade>();
+
+        ObservableCollection<Bairro> lista_bairros = new ObservableCollection<Bairro>();
+
         public BairrosPorCidade()
         {
             InitializeComponent();
+
+            pck_cidade.ItemsSource = lista_cidades;
+
+            lst_bairros.ItemsSource = lista_bairros;
         }
 
-        private void pck_estado_SelectedIndexChanged(object sender, EventArgs e)
+        private async void pck_estado_SelectedIndexChanged(object sender, EventArgs e)
         {
+            try
+            {
+                Picker disparador = sender as Picker;
 
+                string estado_selecionado = disparador.SelectedItem as string;
+
+                List<Cidade> arr_cidades = await DataService.GetCidadesByUf(estado_selecionado);
+
+                lista_cidades.Clear();
+
+                arr_cidades.ForEach(i => lista_cidades.Add(i));
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops", ex.Message, "OK");
+            }
         }
 
         private void pck_cidade_SelectedIndexChanged(object sender, EventArgs e)
